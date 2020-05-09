fbda2140a617249720782f0e876f57114e355847
Admin <Admin@JEET>
2020-05-09 19:38:12 +0530

    2nd

--- egazette/src/main/java/com/egazette/Egazette.java
+++ egazette/src/main/java/com/egazette/Egazette.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+
+
 public class Egazette {
 @FindBy(xpath="//*[@id=\"ImgMessage_OK\"]")
 private WebElement attention;
@@ -18,6 +20,13 @@ private WebElement attention;
 private WebElement directory;
 @FindBy(id="ddlCategory")
 private WebElement lb1;
+@FindBy(id="ddlPartSection")
+private WebElement lb2;
+@FindBy(id="btnSubmit")
+private WebElement submitbtn;
+@FindBy(xpath="/html/body/form/center/div/table[4]/tbody/tr[1]/td/div/table/tbody/tr[17]/td/table/tbody/tr/td[2]/a")
+private WebElement pg2;
+
 public Egazette(WebDriver driver)
 {
 	PageFactory.initElements(driver, this);
@@ -38,6 +47,22 @@ public void egazActions()
 	lb1.click();
 	Select s= new Select(lb1);
 	s.selectByVisibleText("Weekly Gazette");
-			
-	 }
+	
+	lb2.click();
+	Select s1=new Select(lb2);
+ s1.selectByVisibleText("Part I-Section 1");
+ 
+ try {
+	Thread.sleep(1000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+ submitbtn.click();
+ 
+ pg2.click();
+
+ 
+
+		 }
 }
